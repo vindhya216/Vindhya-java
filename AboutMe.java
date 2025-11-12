4d15c3e30e7778198435e671716603ec20743102
vindhya216 <vindhyashankar21@gmail.com>
2025-11-12 18:23:00 +0530

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,10 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "M S Vindhya";
+        int age = 19;
+        String language = "Java";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Favorite Language: " + language);
+    }
+}
