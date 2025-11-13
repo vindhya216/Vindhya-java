083ef9cb37f5fc65e316e5dd2a2a8c67e1c04c7c
vindhya216 <vindhyashankar21@gmail.com>
2025-11-13 18:06:31 +0530

    Add files via upload

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,8 +1,19 @@
+
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int num = 5;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("\nMultiplication Table of " + num + ":");
+
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " x " + i + " = " + (num * i));
         }
+
+        sc.close();
     }
 }
