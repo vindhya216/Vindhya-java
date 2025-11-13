6931af5f9de6944b03d27dfb742f666f29c05109
vindhya216 <vindhyashankar21@gmail.com>
2025-11-13 18:15:09 +0530

    Add files via upload

---
+++ SkipFive.java
@@ -0,0 +1,10 @@
+public class SkipFive {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; // skip 5
+            }
+            System.out.println(i);
+        }
+    }
+}
